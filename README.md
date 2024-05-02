2ab35dd40e3b3db23ee29007eef412d7b8bf05de
Taninu <martinkrieg@Air-von-Martin.fritz.box>
2024-05-02 22:28:56 +0200

    some explainations

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ## Software Requirements
 
 - Docker
+- Python
+
+## Install requirements
+
+- pip install -r requirements.txt
+  
+## docker commands
+  
+- docker build -t <image_name> .
+- docker run -p 8080:8080 <image_name>
--- model_container/model_execution.py
+++ model_container/model_execution.py
@@ -1,16 +1,12 @@
 import keras
 import tensorflow as tf
-import numpy as np
+import numpy as np 
 from typing import Any
-
 
 def load_model():
     return keras.saving.load_model("dummy_model/test.keras")
 
-
-def load_and_convert_image(
-    img_path: str, img_height: int, img_width: int
-) -> np.ndarray:
+def load_and_convert_image(img_path:str, img_height:int, img_width:int) -> np.ndarray:
     """This function loads a image and converted it into a np.ndarray
     Args:
         img_path (str): image location
@@ -20,17 +16,18 @@ def load_and_convert_image(
     Returns:
         np.ndarray: converted image-array
     """
-
+    
     img = tf.keras.utils.load_img(
-        img_path, target_size=(img_height, img_width), color_mode="grayscale"
+        img_path, 
+        target_size=(img_height, img_width),
+        color_mode="grayscale"
     )
     img_array = tf.keras.utils.img_to_array(img)
-    img_array = img_array.reshape((-1, img_height * img_width))
-
+    img_array = img_array.reshape((-1, img_height*img_width))
+    
     return img_array
 
-
-def predict(model: Any, img_array: np.ndarray) -> int:
+def predict(model:Any, img_array: np.ndarray) -> int:
     """This function crates a prediction of a given np.ndarray
 
     Args:
@@ -41,7 +38,7 @@ def predict(model: Any, img_array: np.ndarray) -> int:
         int: predicted class
     """
     pred = model.predict(img_array)
-
+ 
     score = tf.nn.softmax(pred[0])
     result = int([np.argmax(score)][0])
 
