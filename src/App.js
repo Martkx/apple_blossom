7791a2aa326c04471d9de7564b5ac3f2f0e1f0c8
Jabe02 <166744911+Jabe02@users.noreply.github.com>
2024-05-11 19:00:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Header from './Components/Header.js';
+import Dashboard from './Components/dashboard.js';
+import Sidebar from './Components/sidebar.js';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Header/>
+<div className='container'>
+     <Sidebar/> <div className='main-content'>
+      <Routes>
+         <Route path="/dashboard" element={<Dashboard/>}></Route></Routes>
+      </div>
+        </div> 
+        </div>   
+      </Router>    
+      
+     
+   
+  );
+}
+
+export default App;
---
+++ src/Components/dashboard.js
@@ -0,0 +1,68 @@
+
+import './dashboard.css';
+import React, { useState, useRef } from 'react';
+
+function Dashboard() {
+  const [dragging, setDragging] = useState(false);
+  const [file, setFile] = useState(null);
+  const inputRef = useRef(null);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+
+    const droppedFile = e.dataTransfer.files[0];
+    setFile(droppedFile);
+  };
+
+  const handleInputChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleRemoveImage = () => {
+    setFile(null);
+  };
+
+  return (
+    <div>
+      <div className='card'>
+        <h2 className='disclaimer'>Disclaimer: Only the BBCH stages from 57 to 71 can be detected</h2>
+      </div>
+      <div className='card'>
+        <h2>Upload your File</h2>
+        <div
+          className={`drag-area ${dragging ? 'active' : ''}`}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
+          {file && <><img src={URL.createObjectURL(file)} alt="uploaded" style={{ width: 'auto', maxHeight: '80%' }}/>
+                      <button className='removeButton' onClick={handleRemoveImage}>Remove</button></>} 
+          {!file && (
+            <>
+              <div className='header'>Drag & Drop to Upload File</div>
+              <button onClick={() => inputRef.current.click()}>Or click to select file</button>
+              <input ref={inputRef} type="file" accept="image/*" onChange={handleInputChange} style={{ display: 'none' }} />
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
+
+
+
+  
