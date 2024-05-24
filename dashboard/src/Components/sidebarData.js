c5c4add950543efe00b28160d05c0394457b7807
jasmin <jasmin@example.de>
2024-05-24 18:14:38 +0200

    Fehler Fix chatbot

---
+++ dashboard/src/Components/sidebarData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+export const sidebarData = [
+  {
+    
+    title: "Dashboard",
+    icon: <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>,
+    link: "/"
+  
+  }]
+
+
+
