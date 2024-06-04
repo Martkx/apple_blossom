f978fb56f12a7340e328ae4cdfa33369222a174b
Taninu <martinkrieg@Air-von-Martin.fritz.box>
2024-06-04 14:14:53 +0200

    Adjust the BBCH print

--- ki_model/model_execution.py
+++ ki_model/model_execution.py
@@ -3,6 +3,19 @@ import tensorflow as tf
 import numpy as np 
 from typing import Any
 
+# mapping for BBCH-Stage
+map_bbch={0:53,
+          1:54,
+          2:56,
+          3:57,
+          4:59,
+          5:61,
+          6:65,
+          7:67,
+          8:69,
+          9:71,
+          }
+
 def load_model():
     return keras.saving.load_model("ki_model/2.model_developing/model_mobileNet_apples.keras")
 
@@ -29,7 +42,7 @@ def load_and_convert_image(img_path:str, img_height:int, img_width:int) -> np.nd
     
     return img_array
 
-def predict(model:Any, img_array: np.ndarray) -> tuple[int,float]:
+def predict(model:Any, img_array: np.ndarray) -> tuple[int,str]:
     """This function crates a prediction of a given np.ndarray
 
     Args:
@@ -37,12 +50,12 @@ def predict(model:Any, img_array: np.ndarray) -> tuple[int,float]:
         img_array (_type_): converted image_array
 
     Returns:
-        int: predicted class
+        str: predicted class
     """
     pred = model.predict(img_array)
  
     score = tf.nn.softmax(pred[0])
     predicted_class = int([np.argmax(score)][0])
-    probability = round(float(score[predicted_class]),2)
+    probability = round(float(score[predicted_class])*100, 2)
 
-    return predicted_class, probability
+    return map_bbch[predicted_class], f'{probability} %'
