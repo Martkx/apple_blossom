fa0d6b3cd0c00f8fac8e88a2a93b3af2a72302cd
Taninu <martinkrieg@Air-von-Martin.fritz.box>
2024-06-20 21:32:39 +0200

    More details and chatbot

--- ki_model/api.py
+++ ki_model/api.py
@@ -10,26 +10,28 @@ app = FastAPI()
 
 # Configure CORS
 origins = [
-    "http://localhost:8070",  
-    "http://localhost:8080",  
+    "http://localhost:8070",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     # Allows CORS requests from these origins
-    allow_origins=origins,  
+    allow_origins=origins,
     allow_credentials=True,
     # Allows all HTTP methods
-    allow_methods=["*"],  
+    allow_methods=["*"],
     # Allows all HTTP headers
-    allow_headers=["*"],  
+    allow_headers=["*"],
 )
 
 model = model_execution.load_model()
 
+
 @app.get("/")
 async def read_root():
     return {"message": "Model API"}
+
 
 @app.post("/uploadfile")
 async def create_upload_file(file: UploadFile) -> dict[str, Any]:
@@ -52,6 +54,7 @@ async def create_upload_file(file: UploadFile) -> dict[str, Any]:
     except Exception as e:
         return {"message": e.args}
 
+
 @app.get("/prediction")
 async def get_prediction() -> dict[str, Any]:
     """API-Endpoint to get a prediction
@@ -60,27 +63,39 @@ async def get_prediction() -> dict[str, Any]:
     """
     try:
         img_array = model_execution.load_and_convert_image(FILE_PATHS[-1], 224, 224)
-        predicted_class, probability = model_execution.predict(model, img_array)
-
-        return {"predicted_class": predicted_class,
-                "probability": probability}
+        predicted_class, probability, bbch_name, bbch_defintion = (
+            model_execution.predict(model, img_array)
+        )
 
     except Exception as e:
-        return {"message": e.args}
-    
-@app.get("/gradcam") 
+        return {"message": e}
+
+    else:
+        return {
+            "predicted_class": predicted_class,
+            "probability": probability,
+            "name": bbch_name,
+            "definition": bbch_defintion,
+        }
+
+
+@app.get("/gradcam")
 async def get_gradcam():
     """API-Endpoint to get a gradcam image for the prediction
     Returns:
         .jpg
     """
-    img_path_save = 'ki_model/images/grad_cam/grad_cam.jpg'
-    last_conv_layer_name = "conv_pw_13_relu" 
-    img_size= (224, 224)  
+    img_path_save = "ki_model/images/grad_cam/grad_cam.jpg"
+    last_conv_layer_name = "conv_pw_13_relu"
+    img_size = (224, 224)
     try:
-        img_array = gradcam.get_img_array(img_path_gradcam=FILE_PATHS[-1], img_size_gradcam=img_size)
+        img_array = gradcam.get_img_array(
+            img_path_gradcam=FILE_PATHS[-1], img_size_gradcam=img_size
+        )
         heatmap = gradcam.make_gradcam_heatmap(img_array, model, last_conv_layer_name)
         gradcam.save_gradcam(img_path=FILE_PATHS[-1], heatmap=heatmap)
-        return FileResponse(img_path_save, media_type='image/jpeg', filename="grad_cam.jpg")
+        return FileResponse(
+            img_path_save, media_type="image/jpeg", filename="grad_cam.jpg"
+        )
     except Exception as e:
-        return {"message":e.args}
+        return {"message": e.args}
--- ki_model/model_execution.py
+++ ki_model/model_execution.py
@@ -1,25 +1,36 @@
 import keras
 import tensorflow as tf
-import numpy as np 
+import numpy as np
 from typing import Any
+import json
 
 # mapping for BBCH-Stage
-map_bbch={0:53,
-          1:54,
-          2:56,
-          3:57,
-          4:59,
-          5:61,
-          6:65,
-          7:67,
-          8:69,
-          9:71,
-          }
+map_bbch = {
+    0: 53,
+    1: 54,
+    2: 56,
+    3: 57,
+    4: 59,
+    5: 61,
+    6: 65,
+    7: 67,
+    8: 69,
+    9: 71,
+}
+# informations aboout BBCH-stages
+with open("/code/ki_model/BBCH_informations.json", "r") as file:
+    bbch_informations = json.load(file)
+
 
 def load_model():
-    return keras.saving.load_model("ki_model/2.model_developing/model_mobileNet_apples.keras")
+    return keras.saving.load_model(
+        "ki_model/2.model_developing/model_mobileNet_apples.keras"
+    )
 
-def load_and_convert_image(img_path:str, img_height:int, img_width:int) -> np.ndarray:
+
+def load_and_convert_image(
+    img_path: str, img_height: int, img_width: int
+) -> np.ndarray:
     """This function loads a image and converted it into a np.ndarray
     Args:
         img_path (str): image location
@@ -29,20 +40,19 @@ def load_and_convert_image(img_path:str, img_height:int, img_width:int) -> np.nd
     Returns:
         np.ndarray: converted image-array
     """
-    
+
     img = tf.keras.utils.load_img(
-        img_path, 
-        target_size=(img_height, img_width),
-        color_mode="rgb"
+        img_path, target_size=(img_height, img_width), color_mode="rgb"
     )
     img_array = tf.keras.utils.img_to_array(img)
-    #img_array = img_array.reshape((-1, img_height*img_width))
+    # img_array = img_array.reshape((-1, img_height*img_width))
     img_array = np.expand_dims(img_array, axis=0)
     img_array = tf.keras.applications.mobilenet.preprocess_input(img_array)
-    
+
     return img_array
 
-def predict(model:Any, img_array: np.ndarray) -> tuple[int,str]:
+
+def predict(model: Any, img_array: np.ndarray) -> tuple[int, str, int, str]:
     """This function crates a prediction of a given np.ndarray
 
     Args:
@@ -50,12 +60,21 @@ def predict(model:Any, img_array: np.ndarray) -> tuple[int,str]:
         img_array (_type_): converted image_array
 
     Returns:
-        str: predicted class
+        tuple containing
+
+        - bbch_class (int): class number
+        - probability (str): probability of the class
+        - bbch_name (str): name of the class
+        - bbch_definition (str): definition of the class
     """
     pred = model.predict(img_array)
- 
+
     score = tf.nn.softmax(pred[0])
     predicted_class = int([np.argmax(score)][0])
-    probability = round(float(score[predicted_class])*100, 2)
+    probability = round(float(score[predicted_class]) * 100, 2)
 
-    return map_bbch[predicted_class], f'{probability} %'
+    bbch_class = map_bbch[predicted_class]
+    bbch_name = bbch_informations[str(bbch_class)]["name"]
+    bbch_definition = bbch_informations[str(bbch_class)]["definition"]
+
+    return bbch_class, f"{probability} %", bbch_name, bbch_definition
